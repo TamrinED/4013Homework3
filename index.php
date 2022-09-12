d912595ecc9375eef609678045e20d8f4e5e74e7
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-12 14:34:31 -0500

    Update index.php
    copied/pasted code from hw2

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+<?php require_once("header.php"); ?>
+<h1>My Website</h1>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
+<form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
 
 <?php
 $servername = "localhost";
@@ -27,3 +21,20 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<?php require_once("footer.php"); ?>
