f6358eba160cff3499f6798d027aed27ef7e81ab
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-12 14:28:48 -0500

    Update index.php
    dunnno

--- index.php
+++ index.php
@@ -12,3 +12,18 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
