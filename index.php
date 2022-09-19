af738c37120d1590aa7a2c9e1238a880ebb7e48d
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-19 14:18:40 -0500

    Update index.php
    changed credentials

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
   <tbody>
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "tamrined_suser";
+$password = "(_y)XTDI)NmV";
+$dbname = "tamrined_4013Homework3";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
