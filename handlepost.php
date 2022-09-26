d77985688e09c06f752d391cc517348ed17cc45e
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-26 13:42:55 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,5 +1,6 @@
-<!doctype html>
-<html lang="en">
+<?php
+require_once("header.php"); ?>
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,3 +15,6 @@ Your email address is: <?php echo $_GET["instructor"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php
+require_once("footer.php"); ?>
