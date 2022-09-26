72c524919e9911798b4e26646e918eabcf4ee922
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-26 14:14:48 -0500

    Update instructor-cards.php

--- instructor-cards.php
+++ instructor-cards.php
@@ -6,9 +6,9 @@ require_once("header.php"); ?>
 <div class="card-group">
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "tamrined_suser";
+$password = "(_y)XTDI)NmV";
+$dbname = "tamrined_4013Homework3";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
