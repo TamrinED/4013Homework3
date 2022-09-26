f8bcdd3163acab1e547a9d5a9fc126ae660cc4f1
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-09-26 14:09:09 -0500

    Create instructor-cards.php

---
+++ instructor-cards.php
@@ -0,0 +1,52 @@
+<?php
+require_once("header.php"); ?>
+
+<body>
+    <h1>Instructors</h1>
+<div class="card-group">
+    <?php
+$servername = "localhost";
+$username = "projecto_homework3";
+$password = "0w_zeP}]OVy0";
+$dbname = "projecto_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT instructor_id, instructor_name from instructor";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["instructor_name"]?></h5>
+      <p class="card-text"><ul>
+<?php
+    $section_sql = "select c.description from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where i.instructor_id=" . $row["instructor_id"];
+    $section_result = $conn->query($section_sql);
+    
+    while($section_row = $section_result->fetch_assoc()) {
+      echo "<li>" . $section_row["description"] . "</li>";
+    }
+?>
+      </ul></p>
+  </div>
+    </div>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </card-group>
+
+<?php
+require_once("footer.php"); ?>
