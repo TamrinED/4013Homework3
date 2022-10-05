b923737005e0edbd073949fccbd78cbe19620736
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-10-05 09:04:22 -0500

    Update course-section.php

--- course-section.php
+++ course-section.php
@@ -26,7 +26,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$cid = $_POST['courseid'];
+$cid = $_POST['sectionid'];
 //echo $iid;
 $sql = "SELECT * 
 FROM section s join instructor i on i.instructorid = s.instructorid join course c on c.courseid = s.courseid where c.courseid=s.courseid" . $cid;
