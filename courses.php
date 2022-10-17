983a82ca252d96a19bae5c547b8828932f28d9b9
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-10-17 08:45:01 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -23,9 +23,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$cid = $_POST['Instructor.InstructorID'];
+
     
-$sql = "SELECT CourseID, Prefix, Number FROM Course c JOIN Instructor i ON c.InstructorID=i.InstructorID WHERE Course.InstructorID =" $cid;
+$sql = "SELECT CourseID, Prefix, Number FROM Course c JOIN Instructor i ON c.InstructorID=i.InstructorID WHERE Course.InstructorID =Instructor.InstructorID";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -37,7 +37,7 @@ if ($result->num_rows > 0) {
     <td><?=$row["Prefix"]?></td>
     <td><?=$row["Number"]?></td>
     
-      <form method="post" action="course-section.php">
+      <form method="post" action="instructor-course.php">
         <input type="hidden" name="CourseID" value="<?=$row["CourseID"]?>" />
         <input type="submit" value="Sections" />
       </form>
