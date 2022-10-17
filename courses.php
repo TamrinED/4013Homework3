e6b3c6c4ae67d3dd2e6070f2451a05a5000908b5
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-10-17 07:49:24 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -24,7 +24,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Course.CourseID, Course.Prefix, Course.Number FROM Course c Join Section s On c.CourseID=s.CourseID WHERE s.CourseID = c.CourseID";
+$sql = "SELECT Course.CourseID, Course.Prefix, Course.Number FROM Course c Join Section s On c.CourseID=s.CourseID";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
