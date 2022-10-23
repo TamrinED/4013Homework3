4a7be292cb75e7a707ab5b9f041ca6e4ade2bfc8
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-10-23 15:10:32 -0500

    Update related.php

--- related.php
+++ related.php
@@ -33,7 +33,7 @@ if ($result->num_rows > 0) {
     $section_result = $conn->query($section_sql);
     
     while($section_row = $section_result->fetch_assoc()) {
-      echo "<li>" . $section_row["Prefix"] . " " . $section_row["Number"]"</li>";
+      echo "<li>" . $section_row["Prefix"] . " " . $section_row["Number"]" . </li>";
     }
 ?>
       </ul></p>
