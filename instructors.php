0be79dd8e6d38711647ca1102d339084a7f81bb5
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-10-23 14:03:45 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -34,7 +34,7 @@ if ($result->num_rows > 0) {
   <tr>
     <td><?=$row["InstructorID"]?>
       <form method="post" action="instructor-course.php">
-                <input type="hidden" name="ciid" value="<?=$row["InstructorID"]?>" />
+                <input type="submit" name="ciid" value="<?=$row["InstructorID"]?>" />
                
               </form></td>
     <td><?=$row["FirstName"]?></td>
